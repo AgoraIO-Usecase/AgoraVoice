930073265007435ea0d0626944319910ee40db8e
brianxcli <neteasy1210@gmail.com>
2020-09-04 19:00:58 +0800

    nickname edit UI

--- AgoraVoice_Android/app/src/main/java/io/agora/agoravoice/ui/activities/NicknameEditActivity.java
+++ AgoraVoice_Android/app/src/main/java/io/agora/agoravoice/ui/activities/NicknameEditActivity.java
@@ -1,4 +1,44 @@
 package io.agora.agoravoice.ui.activities;
 
-public class NicknameEditActivity extends BaseActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import io.agora.agoravoice.R;
+import io.agora.agoravoice.utils.WindowUtil;
+
+public class NicknameEditActivity extends BaseActivity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowUtil.hideStatusBar(getWindow(), false);
+        setContentView(R.layout.activity_edit_nickname);
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.main_profile_edit_confirm).setOnClickListener(this);
+        findViewById(R.id.main_profile_edit_cancel).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onGlobalLayoutCompleted() {
+        RelativeLayout topLayout = findViewById(R.id.name_edit_top_color_layout);
+        if (topLayout != null) {
+            RelativeLayout.LayoutParams params =
+                    (RelativeLayout.LayoutParams) topLayout.getLayoutParams();
+            params.topMargin += systemBarHeight;
+            topLayout.setLayoutParams(params);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.main_profile_edit_confirm:
+            case R.id.main_profile_edit_cancel:
+                break;
+        }
+    }
 }
