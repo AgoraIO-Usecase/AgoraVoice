af2920f5db03b37c936207e58ce1346f1607a263
CavanSu <suruoxi@agora.io>
2020-12-31 15:43:06 +0800

    build 153

--- AgoraVoice-iOS/AgoraVoice/AgoraVoice/Utils/AppDelegate.swift
+++ AgoraVoice-iOS/AgoraVoice/AgoraVoice/Utils/AppDelegate.swift
@@ -7,13 +7,23 @@
 //
 
 import UIKit
+import Bugly
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        enableBugly()
+        
         return true
+    }
+    
+    func enableBugly() {
+        let config = BuglyConfig()
+        let buglyId = Keys.BuglyId
+        Bugly.start(withAppId: buglyId,
+                    config: config)
     }
 }
 
